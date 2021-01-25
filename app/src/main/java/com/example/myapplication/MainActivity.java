7ea6ccb45b2d1e1016c0b1635431f94f00f86495
stud <stud@almetpt.local>
2021-01-25 10:55:00 +0300

    aaaaa

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textDate=findViewById(R.id.TDate);
+        SimpleDateFormat format= new SimpleDateFormat("dd.MM.yyyy");
+        textDate.setText(format.format(new Date()));
     }
+
+    public void enter(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.Theme_AppCompat_Dialog_Alert);
+        LayoutInflater inflater = getLayoutInflater();
+        View v = inflater.inflate(R.layout.registation_form, null);
+
+        final TextView textView = v.findViewById(R.id.cancel);
+        builder.setView(v).setCancelable(true);
+        final AlertDialog alertDialog = builder.create();
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
+
 }
